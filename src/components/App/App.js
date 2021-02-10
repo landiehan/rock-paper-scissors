c1c87cee59acdc1a54d35b09788287fbee92d08b
Landay <67254560+landiehan@users.noreply.github.com>
2021-02-10 23:51:25 +0800

    add base styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Playground from "../Playground/Playground";
+import Scoreboard from "../Scoreboard/Scoreboard";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [score, setScore] = useState(0);
+
+  return (
+    <div className="App">
+      <Scoreboard score={score} />
+      <Playground setScore={setScore} />
+    </div>
+  );
 }
 
 export default App;
